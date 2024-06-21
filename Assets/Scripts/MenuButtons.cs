31a7bb63dd9d756c4124579a1b9b9cea951f5fca
nrtqq <nrtqqgaming@gmail.com>
2024-06-22 00:56:01 +0200

    Add Menu Background Animatons Add Start Game Animation

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
 public class MenuButtons : MonoBehaviour
-{ 
+{
+    public float delayTime = 4f;
     public void GStart()
     {
-        SceneManager.LoadScene(1);
+        Invoke("PerformAction", delayTime);
     }
     public void Exit()
     {
         Application.Quit();
+    }
+    void PerformAction()
+    {
+        SceneManager.LoadScene(1);
     }
 }
