179001421fef9851c2f12fb29572ebedad6d7fe6
nrtqq <nrtqqgaming@gmail.com>
2024-06-29 17:36:59 +0200

    Add AttackAnimation

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("isAttack");
+        }
+        
+    }
+}
